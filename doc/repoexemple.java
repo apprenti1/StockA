fb21ab99a9c6b0022dc7810a61391a5ca5e0c910
Nicolas <nicolas.cataluna@gmail.com>
2024-01-15 11:36:45 +0100

    upl : repoexemple.java
    in work

---
+++ doc/repoexemple.java
@@ -0,0 +1,16 @@
+public class Name{
+
+    //exemple
+    public Name(String firstName, int age, String country) {
+
+        this.firstName = firstName;
+        this.age = age;
+        this.country = country;
+    }
+    //exemple
+
+    public Name(Entity en parametre) {
+        inserte/update/delete/select/...
+    }
+
+}
