bb95269be67be891b6cb84bf07cdd0e218f47b78
megtaho <122614093+megtaho@users.noreply.github.com>
2024-01-27 19:09:08 +0100

    add Page Admin

---
+++ src/main/java/controller/PageAdminController.java
@@ -0,0 +1,69 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class PageAdminController {
+
+    @FXML
+    private Label nomEleve;
+
+    @FXML
+    private VBox VboxAttente;
+
+    @FXML
+    private VBox VboxConfirme;
+
+    @FXML
+    private VBox VboxArchive;
+
+    @FXML
+    private TextField inputNom;
+
+    @FXML
+    private TextField inputPrenom;
+
+    @FXML
+    private TextField inputEmail;
+
+    @FXML
+    private TextField inputTel;
+
+    @FXML
+    private TextField inputAdresse;
+
+    @FXML
+    private TextField inputMotdepasse;
+
+    @FXML
+    private Button creerProf;
+
+    @FXML
+    private Button creerSecretaire;
+
+    @FXML
+    private Button creerGestionnaire;
+
+    @FXML
+    void logoButton() {
+        // Gérer l'action du bouton logoButton
+    }
+
+    @FXML
+    void creerProf() {
+        // Gérer l'action du bouton creerProf
+    }
+
+    @FXML
+    void creerSecretaire() {
+        // Gérer l'action du bouton creerSecretaire
+    }
+
+    @FXML
+    void creerGestionnaire() {
+        // Gérer l'action du bouton creerGestionnaire
+    }
+}
