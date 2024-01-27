785039527e59adc702f8ddb8daf5c707e8474053
megtaho <122614093+megtaho@users.noreply.github.com>
2024-01-27 19:11:35 +0100

    fix accueil- PageAdmin

--- src/main/java/controller/Accueil.java
+++ src/main/java/controller/Accueil.java
@@ -2,29 +2,57 @@ package controller;
 
 import application.Main;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
 public class Accueil {
 
-    @FXML private Button connexion;
-    @FXML private Button editProfil;
-    @FXML private Button inscription;
-    @FXML private Button mineListes;
-    @FXML private Button mineTypes;
+    @FXML
+    private Button connexion;
+    @FXML
+    private Button editProfil;
+    @FXML
+    private Button inscription;
+    @FXML
+    private Button mineListes;
+    @FXML
+    private Button mineTypes;
 
 
+    public Accueil() {
+    }
 
-    public Accueil(){}
-    public void initialize(){}
-    @FXML void switchAccueil(MouseEvent event) {}
-    @FXML void switchConnexion(ActionEvent event) {
+    public void initialize() {
+    }
+
+    @FXML
+    void switchAccueil(MouseEvent event) {
+    }
+
+    @FXML
+    void switchConnexion(ActionEvent event) {
         Main.changeScene("Connexion", new ConnexionController(), "Connexion");
     }
-    @FXML void switchInscription(ActionEvent event) {}
-    @FXML void switchEditProfil(ActionEvent event) {}
-    @FXML void switchMineListes(ActionEvent event) {}
-    @FXML void switchMineTypes(ActionEvent event) {}
+    @FXML private Button admin;
+    @FXML
+    void switchToPageAdmin(ActionEvent event) {
+        Main.changeScene("PageAdmin", new PageAdminController(), "PageAdmin");
+    }
+
+    @FXML
+    void switchInscription(ActionEvent event) {
+    }
+
+    @FXML
+    void switchEditProfil(ActionEvent event) {
+    }
+
+    @FXML
+    void switchMineListes(ActionEvent event) {
+    }
+
+    @FXML
+    void switchMineTypes(ActionEvent event) {
+    }
 }
