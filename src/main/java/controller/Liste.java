cf04d5e6560c9d2b4b7ec473215977206bc630ff
Nicolas <nicolas.cataluna@gmail.com>
2024-02-05 11:08:32 +0100

    add : ControllerListe
    creation du controllerListe avec

---
+++ src/main/java/controller/Liste.java
@@ -0,0 +1,59 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+
+public class Liste {
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button deconnection;
+
+    @FXML
+    private Button editProfil;
+
+    @FXML
+    private ListView<?> liste;
+
+    @FXML
+    private Button mesliste;
+
+    @FXML
+    private ScrollPane principalPanel;
+
+    @FXML
+    private Label titre;
+
+    @FXML
+    void add(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchAccueil(MouseEvent event) {
+
+    }
+
+    @FXML
+    void switchConnexion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switcheditprofil(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchmeslistes(ActionEvent event) {
+
+    }
+
+}
