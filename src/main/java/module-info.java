32aaadded516138c87a284155615d160004c303c
megtaho <122614093+megtaho@users.noreply.github.com>
2024-02-23 16:30:21 +0100

    add dependance

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,16 @@ module lasuperteam.stocka {
     requires javafx.fxml;
     requires org.kordamp.bootstrapfx.core;
     requires java.sql;
+    requires jakarta.mail;
+    requires org.apache.commons.lang3;
+    requires MaterialFX;
 
     opens application to javafx.fxml;
     exports application;
     opens controller to javafx.fxml;
     exports controller;
+    exports bdd;
+    opens bdd to javafx.fxml;
+    exports security;
+    opens security to javafx.fxml;
 }
